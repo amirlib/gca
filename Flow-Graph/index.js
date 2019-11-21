f4df5c69384699b34e4818434aef9c000e3db479
Amir Liberzon <amir.lib@gmail.com>
2019-11-21 20:34:20 +0200

    Improve the code readability of FlowGraph class

--- Flow-Graph/index.js
+++ Flow-Graph/index.js
@@ -3,13 +3,14 @@ const Graph = require("../Graph");
 const LinkedList = require("../LinkedList");
 const FlowEdge = require("../Edges/FlowEdge");
 /**
- * Implementaion of Flow Graph. There are tools for building a Flow Graph that will return a correct max flow from Edmonds Krap alogritem.
+ * Implementation of Flow Graph. There are tools for building a Flow Graph that will return a correct max flow from Edmonds Krap algorithm.
  * @class FlowGraph
  * @extends {Graph}
  */
 class FlowGraph extends Graph {
   constructor() {
     super();
+
     this.s = 0;
     this.t = 1;
     this.edgesList = new LinkedList(); //Save the edges of the graph.
@@ -17,25 +18,27 @@ class FlowGraph extends Graph {
     super.addNode(this.t);
   }
   /**
-   * Marks in the Graph's matrix new edge. USE it only when use need to update the forward edge that was deleted beacuse the capacity of the edge was full.
+   * Marks in the Graph's matrix new edge. USE it only when use need to update the forward edge that was deleted because the capacity of the edge was full.
    * @param {number} from ID of start node edge.
    * @param {number} to ID of the end node edge.
    * @memberof FlowGraph
    */
   markEdge(from, to) {
-    let fromIndex = this.indexOfNodeID(from);
-    let toIndex = this.indexOfNodeID(to);
+    const fromIndex = this.indexOfNodeID(from);
+    const toIndex = this.indexOfNodeID(to);
+
     this.matrix[fromIndex][toIndex] = 1;
   }
   /**
-   * Unmarks in the Graph's matrix the edge. USE it only when use need to update the forward edge that was deleted beacuse the capacity of the edge was full.
+   * Unmarks in the Graph's matrix the edge. USE it only when use need to update the forward edge that was deleted because the capacity of the edge was full.
    * @param {number} from ID of start node edge.
    * @param {number} to ID of the end node edge.
    * @memberof FlowGraph
    */
   unmarkEdge(from, to) {
-    let fromIndex = this.indexOfNodeID(from);
-    let toIndex = this.indexOfNodeID(to);
+    const fromIndex = this.indexOfNodeID(from);
+    const toIndex = this.indexOfNodeID(to);
+
     this.matrix[fromIndex][toIndex] = 0;
   }
   /**
@@ -45,10 +48,9 @@ class FlowGraph extends Graph {
    * @memberof FlowGraph
    */
   addNode(ID) {
-    if (ID != 0 && ID != 1) {
-      return super.addNode(ID);
-    }
-    return false;
+    if (ID == 0 || ID == 1) return false;
+
+    return super.addNode(ID);
   }
   /**
    * Deletes given node ID from the graph except node ID: 0 and 1. Deletes also all the edges that related to this ID.
@@ -57,23 +59,28 @@ class FlowGraph extends Graph {
    * @memberof FlowGraph
    */
   deleteNode(ID) {
-    if (ID != 0 && ID != 1 && this.hasNode(ID)) {
-      let endNodesEdges = this.findEndNodesEdgesFromNode(ID);
-      for (let i = 0; i < endNodesEdges.length; i++) {
-        this.deleteEdge(ID, endNodesEdges[i]);
-      }
-      let stratNodesEdges = this.findStartNodesEdgesFromNode(ID);
-      for (let i = 0; i < stratNodesEdges.length; i++) {
-        this.deleteEdge(stratNodesEdges[i], ID);
-      }
-      super.deleteNode(ID);
-      return true;
+    if (ID == 0 || ID == 1 || !this.hasNode(ID)) return false;
+
+    const endNodesEdges = this.findEndNodesEdgesFromNode(ID);
+    const startNodesEdges = this.findStartNodesEdgesFromNode(ID);
+    let iterator = endNodesEdges.values();
+
+    for (const value of iterator) {
+      this.deleteEdge(ID, value);
     }
-    return false;
+
+    iterator = startNodesEdges.values();
+
+    for (const value of iterator) {
+      this.deleteEdge(value, ID);
+    }
+
+    super.deleteNode(ID);
+    return true;
   }
   /**
    * Inserts new edge to the graph - When '1' in (from, to) represent an edge from startNodeID to endNodeID, and also create an object for the edge.
-   * @param {number} from ID of strat Node of edge.
+   * @param {number} from ID of start Node of edge.
    * @param {number} to ID of end Node of edge.
    * @param {number} capacity The capacity of edge. Default value is 1.
    * @param {number} flow The flow of edge. Default value is 0.
@@ -81,26 +88,25 @@ class FlowGraph extends Graph {
    * @memberof FlowGraph
    */
   addEdge(from, to, capacity = 1, flow = 0) {
-    if (super.addEdge(from, to) == true) {
-      this.edgesList.addData(new FlowEdge(from, to, capacity, flow));
-      return true;
-    }
-    return false;
+    if (!super.addEdge(from, to)) return false;
+
+    this.edgesList.addData(new FlowEdge(from, to, capacity, flow));
+    return true;
   }
   /**
    * Deletes edge from the graph.
-   * @param {number} from ID of strat Node of edge.
+   * @param {number} from ID of start Node of edge.
    * @param {number} to ID of end Node of edge.
    * @returns {boolean} True, if edge deleted successfully from the graph. Otherwise, returns false.
    * @memberof FlowGraph
    */
   deleteEdge(from, to) {
-    if (super.deleteEdge(from, to) == true) {
-      const edge = this.getEdge(from, to, this.edgesList);
-      this.edgesList.removeData(edge);
-      return true;
-    }
-    return false;
+    if (!super.deleteEdge(from, to)) return false;
+
+    const edge = this.getEdge(from, to, this.edgesList);
+
+    this.edgesList.removeData(edge);
+    return true;
   }
   /**
    * Returns an edge that has the same 'from' and 'to' nodes as were given.
@@ -111,13 +117,16 @@ class FlowGraph extends Graph {
    */
   getEdge(from, to) {
     let current = this.edgesList.head;
+
+    if (current == null) return null;
+
     while (current != null) {
       if (current.data.from == from && current.data.to == to) {
         return current.data;
       }
+
       current = current.next;
     }
-    return null;
   }
   /**
    * Resets the flow of all edges in the graph.
@@ -125,6 +134,7 @@ class FlowGraph extends Graph {
    */
   reset() {
     let current = this.edgesList.head;
+
     while (current != null) {
       current.data.resetFlow();
       current = current.next;
@@ -137,9 +147,11 @@ class FlowGraph extends Graph {
    */
   clone() {
     let cloneGraph = new FlowGraph();
+
     for (let i = 0; i < this.matrix.length; i++) {
       cloneGraph.matrix[i] = Array.from(this.matrix[i]);
     }
+    
     cloneGraph.nodesID = Array.from(this.nodesID);
     cloneGraph.edgesList = this.edgesList.clone();
     return cloneGraph;
--- example.js
+++ example.js
@@ -1,45 +1,50 @@
 const gca = require("./index");
 const tool = new gca();
 
-let graph = tool.CreateGraph();
-// let flowGraph = tool.CreateFlowGraph();
+// let graph = tool.CreateGraph();
+let flowGraph = tool.CreateFlowGraph();
 
-graph.addNode(2);
-graph.addNode(3);
-graph.addNode(4);
-graph.addNode(5);
-graph.addEdge(2, 3);
-graph.addEdge(2, 4);
-graph.addEdge(3, 2);
-graph.addEdge(3, 5);
-graph.addEdge(4, 3);
-graph.addEdge(5, 4);
-graph.addEdge(5, 5);
-
-console.log(`${graph.toString()}\n`);
-console.log(`${graph.printNodesID()}\n`);
-
-// flowGraph.addEdge(flowGraph.s, 2, 16);
-// flowGraph.addEdge(flowGraph.s, 3, 13);
-// flowGraph.addEdge(2, 3, 10);
-// flowGraph.addEdge(2, 4, 12);
-// flowGraph.addEdge(3, 2, 4);
-// flowGraph.addEdge(3, 5, 14);
-// flowGraph.addEdge(4, 3, 9);
-// flowGraph.addEdge(4, flowGraph.t, 20);
-// flowGraph.addEdge(5, 4, 7);
-// flowGraph.addEdge(5, flowGraph.t, 4);
-
+// graph.addNode(2);
+// graph.addNode(3);
+// graph.addNode(4);
+// graph.addNode(5);
+// graph.addEdge(2, 3);
+// graph.addEdge(2, 4);
+// graph.addEdge(3, 2);
+// graph.addEdge(3, 5);
+// graph.addEdge(4, 3);
+// graph.addEdge(5, 4);
+// graph.addEdge(5, 5);
 
 // console.log(`${graph.toString()}\n`);
 // console.log(`${graph.printNodesID()}\n`);
-// console.log(`${graph.countEdges()}\n`);
 
-// console.log(flowGraph.findEndNodesEdgesFromNode(2));
-// console.log(tool.EdmondsKarp(flowGraph));
+flowGraph.addNode(2);
+flowGraph.addNode(3);
+flowGraph.addNode(4);
+flowGraph.addNode(5);
+flowGraph.addEdge(flowGraph.s, 2, 16);
+flowGraph.addEdge(flowGraph.s, 3, 13);
+flowGraph.addEdge(2, 7, 10);
+flowGraph.addEdge(2, 4, 12);
+flowGraph.addEdge(3, 2, 4);
+flowGraph.addEdge(3, 5, 14);
+flowGraph.addEdge(4, 3, 9);
+flowGraph.addEdge(4, flowGraph.t, 20);
+flowGraph.addEdge(5, 4, 7);
+flowGraph.addEdge(5, flowGraph.t, 4);
 
-let bfsG = tool.BFS(graph, 2);
-console.log(`${bfsG.toString()}\n`);
+console.log(`${flowGraph.toString()}\n`);
+console.log(`${flowGraph.printNodesID()}\n`);
+
+// console.log(`${flowGraph.deleteNode(5)}\n`);
+// console.log(`${flowGraph.toString()}\n`);
+// console.log(`${flowGraph.printNodesID()}\n`);
+
+console.log(tool.EdmondsKarp(flowGraph));
+
+// let bfsG = tool.BFS(graph, 2);
+// console.log(`${bfsG.toString()}\n`);
 
 // try {
 //     let path = bfsG.getPath(5);
