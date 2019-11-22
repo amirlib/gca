f18d2c83c9f697ba79622b3208cf5667e5a6d727
Amir Liberzon <amir.lib@gmail.com>
2019-11-22 12:44:31 +0200

    Edit example.js file

--- example.js
+++ example.js
@@ -1,23 +1,12 @@
-const gca = require("./index");
+const gca = require("gca");
 const tool = new gca();
 
-// let graph = tool.CreateGraph();
+let graph = tool.CreateGraph();
+
+console.log(graph.toString());
+console.log(graph.printNodesID());
+
 let flowGraph = tool.CreateFlowGraph();
-
-// graph.addNode(2);
-// graph.addNode(3);
-// graph.addNode(4);
-// graph.addNode(5);
-// graph.addEdge(2, 3);
-// graph.addEdge(2, 4);
-// graph.addEdge(3, 2);
-// graph.addEdge(3, 5);
-// graph.addEdge(4, 3);
-// graph.addEdge(5, 4);
-// graph.addEdge(5, 5);
-
-// console.log(`${graph.toString()}\n`);
-// console.log(`${graph.printNodesID()}\n`);
 
 flowGraph.addNode(2);
 flowGraph.addNode(3);
@@ -25,7 +14,7 @@ flowGraph.addNode(4);
 flowGraph.addNode(5);
 flowGraph.addEdge(flowGraph.s, 2, 16);
 flowGraph.addEdge(flowGraph.s, 3, 13);
-flowGraph.addEdge(2, 7, 10);
+flowGraph.addEdge(2, 3, 10);
 flowGraph.addEdge(2, 4, 12);
 flowGraph.addEdge(3, 2, 4);
 flowGraph.addEdge(3, 5, 14);
@@ -34,21 +23,18 @@ flowGraph.addEdge(4, flowGraph.t, 20);
 flowGraph.addEdge(5, 4, 7);
 flowGraph.addEdge(5, flowGraph.t, 4);
 
-console.log(`${flowGraph.toString()}\n`);
-console.log(`${flowGraph.printNodesID()}\n`);
+console.log(flowGraph.toString());
+console.log(flowGraph.printNodesID());
 
-// console.log(`${flowGraph.deleteNode(5)}\n`);
-// console.log(`${flowGraph.toString()}\n`);
-// console.log(`${flowGraph.printNodesID()}\n`);
-
+console.log(flowGraph.findEndNodesEdgesFromNode(2));
 console.log(tool.EdmondsKarp(flowGraph));
 
-// let bfsG = tool.BFS(graph, 2);
-// console.log(`${bfsG.toString()}\n`);
+let bfsG = tool.BFS(flowGraph, flowGraph.s);
+console.log(bfsG.toString());
 
-// try {
-//     let path = bfsG.getPath(5);
-//     console.log(path.toString());
-// } catch (error) {
-//     console.log(`${error.message}`);
-// }
+try {
+    let path = bfsG.getPath(5);
+    console.log(path.toString());
+} catch (error) {
+    console.log(`${error.message}`);
+}
